363a93c2f76e2c9673a89bc0c61fcc62a1690b74
INCOREV34 <rfgrobo@gmail.com>
2021-10-03 19:11:17 +0700

    WIP

--- chellage.c
+++ chellage.c
@@ -2,21 +2,16 @@
 #include<math.h>
 
 int main(){
-    int sum, add, add2,add3,add4,mas;
+    int sum, add, add2;
     printf("input1: ");
     scanf("%d",&add);
     printf("input2: ");
     scanf("%d",&add2);
     add = add << 1;
     add2 = add2 >> 2;
+    add = add >> 3;
+    add2 = add2 << 3;
     sum = add ^ add2;
     printf("hasil: %d",sum);
-    add3 = add;
-    add4 = add2;
-    add3 = add3 >> 3;
-    add4 = add4 << 3;
-    mas = add3 | add4;
-    printf("  hasil jika dibalik: %d\n",mas);
-
     return  0;
 }
